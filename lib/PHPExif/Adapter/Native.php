af1cdc9e13b65aa5252ea6f4634e608191834f52
ildyria <beviguier@gmail.com>
2020-03-09 17:53:31 +0100

    fixes complaints

--- lib/PHPExif/Adapter/Native.php
+++ lib/PHPExif/Adapter/Native.php
@@ -208,7 +208,10 @@ class Native extends AdapterAbstract
         // Check data is a valid string
         foreach ($data as $k => $v) {
             // @codeCoverageIgnoreStart
-            if (!mb_check_encoding($v)) $data[$k] = mb_convert_encoding($v,'UTF-8');
+            if (!mb_check_encoding($v))
+            {
+                $data[$k] = mb_convert_encoding($v, 'UTF-8');
+            }
             // @codeCoverageIgnoreEnd
         }
         // var_dump($data);
