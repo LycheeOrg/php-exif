9bc6f8fb746316a7ed1f64a0c5d85fd1b411be17
ildyria <beviguier@gmail.com>
2021-10-31 18:27:10 +0100

    fix deps

--- lib/PHPExif/Reader/Reader.php
+++ lib/PHPExif/Reader/Reader.php
@@ -102,7 +102,7 @@ class Reader implements ReaderInterface
      * @param string $file
      * @return \PHPExif\Exif Instance of Exif object with data
      */
-    public function read(string $file) : Exif|false|string
+    public function read(string $file) : Exif|string|false
     {
         return $this->getAdapter()->getExifFromFile($file);
     }
@@ -113,7 +113,7 @@ class Reader implements ReaderInterface
      * @param string $file
      * @return \PHPExif\Exif Instance of Exif object with data
      */
-    public function getExifFromFile(string $file) : Exif|string
+    public function getExifFromFile(string $file) : Exif|string|false
     {
         return $this->read($file);
     }
